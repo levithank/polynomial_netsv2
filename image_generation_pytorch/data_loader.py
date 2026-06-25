5fc9aeed9fb4ceeeb6e6acaa15632818df0b28b7
Leroy Kang <kleroy23@vt.edu>
2026-06-25 11:41:55 -0400

    changed dataset to circle

--- image_generation_pytorch/data_loader.py
+++ image_generation_pytorch/data_loader.py
@@ -1,24 +1,47 @@
+# import torch
+# import torchvision
+# import torchvision.transforms as transforms
+
+
+# def get_loader(data_path, batch_size, mode, num_workers=4):
+
+#     transform = transforms.Compose(
+#     [transforms.ToTensor(),
+#      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+#     if mode == 'train':
+#         dataset = torchvision.datasets.CIFAR10(root=data_path, train=True,
+#                                             download=True, transform=transform)
+#         dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
+#                                               shuffle=True, num_workers=num_workers)
+
+#     else:
+#         dataset = torchvision.datasets.CIFAR10(root=data_path, train=False,
+#                                                download=True, transform=transform)
+#         dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
+#                                                  shuffle=False, num_workers=num_workers)
+
+#     return dataloader
+
+import numpy as np
 import torch
-import torchvision
-import torchvision.transforms as transforms
 
+class CircleDataset(torch.utils.data.Dataset):
+    def __init__(self, n_points=10000, radius=1.0, noise=0.0):
+        angles = np.random.uniform(0, 2 * np.pi, n_points)
+        x = radius * np.cos(angles)
+        y = radius * np.sin(angles)
+        pts = np.stack([x, y], axis=1) + np.random.normal(0, noise, (n_points, 2))
+        self.data = torch.tensor(pts, dtype=torch.float32)
 
-def get_loader(data_path, batch_size, mode, num_workers=4):
+    def __len__(self):
+        return len(self.data)
 
-    transform = transforms.Compose(
-    [transforms.ToTensor(),
-     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    def __getitem__(self, idx):
+        return self.data[idx]
 
-    if mode == 'train':
-        dataset = torchvision.datasets.CIFAR10(root=data_path, train=True,
-                                            download=True, transform=transform)
-        dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
-                                              shuffle=True, num_workers=num_workers)
+def get_loader(batch_size, n_points=10000, num_workers=4, shuffle=True):
+    dataset = CircleDataset(n_points=n_points)
+    return torch.utils.data.DataLoader(dataset, batch_size=batch_size,
+                                       shuffle=shuffle, num_workers=num_workers)
 
-    else:
-        dataset = torchvision.datasets.CIFAR10(root=data_path, train=False,
-                                               download=True, transform=transform)
-        dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
-                                                 shuffle=False, num_workers=num_workers)
-
-    return dataloader
